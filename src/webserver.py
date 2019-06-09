be625f10417feefcc192f304c1fb55feb6797cc2
a3y3 <sohamssd@gmail.com>
2019-06-09 17:57:34 -0400

    Finish sign in

--- src/webserver.py
+++ src/webserver.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, \
-    session as login_session
+    session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -24,8 +24,16 @@ def create_state():
     state = ''.join(
         random.choice(string.ascii_uppercase + string.ascii_lowercase)
         for x in range(32))
-    login_session['state'] = state
+    session['state'] = state
     return state
+
+
+def valid_state():
+    received_state = request.form['state']
+    if received_state != session['state']:
+        print("CSRF mismatch", received_state, "!=", session['state'])
+        return False
+    return True
 
 
 # =========Index=============
@@ -38,16 +46,17 @@ def index():
 @app.route('/catalogs/', methods=["GET", "POST"])
 def catalogs():
     if request.method == "GET":
-        session = DBSession()
-        catalogs = session.query(Catalog).all()
+        db_session = DBSession()
+        catalogs = db_session.query(Catalog).all()
+        db_session.close()
         return render_template('catalogs/catalogs.html', catalogs=catalogs)
     elif request.method == "POST":
-        session = DBSession()
+        db_session = DBSession()
         name = request.form['name']
         catalog = Catalog(name=name)
-        session.add(catalog)
-        session.commit()
-        session.close()
+        db_session.add(catalog)
+        db_session.commit()
+        db_session.close()
         return redirect(url_for('catalogs'))
 
 
@@ -58,9 +67,9 @@ def new_catalog():
 
 @app.route('/catalogs/<int:catalog_id>')
 def show_catalog(catalog_id):
-    session = DBSession()
-    items = session.query(Item).filter_by(catalog_id=catalog_id).all()
-    session.close()
+    db_session = DBSession()
+    items = db_session.query(Item).filter_by(catalog_id=catalog_id).all()
+    db_session.close()
     return render_template('catalogs/show.html', items=items,
                            catalog_id=catalog_id)
 
@@ -68,34 +77,37 @@ def show_catalog(catalog_id):
 # =========Items=============
 @app.route('/items', methods=['GET', 'POST'])
 def items():
+    print('idinfo' in session)
     if request.method == "GET":
-        session = DBSession()
-        items = session.query(Item).all()
+        db_session = DBSession()
+        items = db_session.query(Item).all()
+        db_session.close()
         return render_template('items/items.html', items=items)
     elif request.method == "POST":
-        session = DBSession()
+        db_session = DBSession()
         name = request.form['name']
         description = request.form['description']
         catalog_id = request.form['catalog_id']
         item = Item(name=name, description=description, catalog_id=catalog_id)
-        session.add(item)
-        session.commit()
-        session.close()
+        db_session.add(item)
+        db_session.commit()
+        db_session.close()
         return redirect(url_for('items'))
 
 
 @app.route('/items/new/')
 def new_item():
-    session = DBSession()
-    catalogs = session.query(Catalog).all()
+    db_session = DBSession()
+    catalogs = db_session.query(Catalog).all()
+    db_session.close()
     return render_template('items/new.html', catalogs=catalogs)
 
 
 @app.route('/items/<int:item_id>')
 def show_item(item_id):
-    session = DBSession()
-    item = session.query(Item).filter_by(item_id=item_id).one()
-    session.close()
+    db_session = DBSession()
+    item = db_session.query(Item).filter_by(item_id=item_id).one()
+    db_session.close()
     return render_template('items/show.html', item=item)
 
 
@@ -104,19 +116,43 @@ def show_item(item_id):
 def login():
     if request.method == "GET":
         state = create_state()
-        client_id = json.loads(open('../secrets/app_secrets.json', 'r').read())['web'][
-            'google']['client_id']
-        return render_template('login/new.html', client_id=client_id)
+        session['state'] = state
+        return render_template('login/new.html', state=state)
+
     if request.method == "POST":
-        pass
+        if not valid_state():
+            return json.dumps({'success': False}), 401, {
+                'ContentType': 'application/json'}
+
+        token = request.form['token']
+        client_id = \
+            json.loads(open('../secrets/app_secrets.json', 'r').read())['web'][
+                'google']["client_id"]
+        try:
+            idinfo = id_token.verify_oauth2_token(token, requests.Request(),
+                                                  client_id)
+            session['idinfo'] = idinfo
+            if idinfo['iss'] not in ['accounts.google.com',
+                                     'https://accounts.google.com']:
+                raise ValueError('Wrong issuer.')
+        except ValueError:
+            print("Raised error")
+            session['idinfo'] = None
+            return json.dumps({'success': False}), 401, {
+                'ContentType': 'application/json'}
+        return json.dumps({'success': True}), 200, {
+            'ContentType': 'application/json'}
+
     elif request.method == "DELETE":
-        pass
+        session['idinfo'] = None
+        return json.dumps({'success': True}), 200, {
+                'ContentType': 'application/json'}
 
 
 if __name__ == '__main__':
     app.debug = True
     secret_key = \
-    json.loads(open('../secrets/app_secrets.json', 'r').read())['app'][
-        'secret']
+        json.loads(open('../secrets/app_secrets.json', 'r').read())['app'][
+            'secret']
     app.secret_key = secret_key
     app.run(host='0.0.0.0', port=5000)
