bad539da30354c3273bf3ef98ee83372b358b06b
a3y3 <sohamssd@gmail.com>
2019-06-09 11:11:59 -0400

    Finish Create and Read for catalogs, start work for items

--- src/webserver.py
+++ src/webserver.py
@@ -15,11 +15,13 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 
 
+# =========Index=============
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+# =========Catalogs=============
 @app.route('/catalogs/', methods=["GET", "POST"])
 def catalogs():
     if request.method == "GET":
@@ -37,7 +39,7 @@ def catalogs():
 
 
 @app.route('/catalogs/new/')
-def create_catalog():
+def new_catalog():
     return render_template('catalogs/new.html')
 
 
@@ -46,10 +48,44 @@ def show_catalog(catalog_id):
     session = DBSession()
     items = session.query(Item).filter_by(catalog_id=catalog_id).all()
     session.close()
-    return render_template('catalogs/show.html', items=catalog_id)
+    return render_template('catalogs/show.html', items=items,
+                           catalog_id=catalog_id)
+
+
+# =========Items=============
+@app.route('/items', methods=['GET', 'POST'])
+def items():
+    if request.method == "GET":
+        session = DBSession()
+        items = session.query(Item).all()
+        return render_template('items/items.html', items=items)
+    elif request.method == "POST":
+        session = DBSession()
+        name = request.form['name']
+        description = request.form['description']
+        catalog_id = request.form['catalog_id']
+        user_id = request.form['user_id']
+        item = Item(name=name, description=description, catalog_id=catalog_id,
+                    user_id=user_id)
+        session.add(item)
+        session.commit()
+        session.close()
+        return redirect(url_for('items'))
+
+
+@app.route('/items/new/')
+def new_item():
+    return render_template('items/new.html')
+
+
+@app.route('/items/<int:item_id>')
+def show_item(item_id):
+    session = DBSession()
+    item = session.query(Item).filter_by(item_id=item_id).one()
+    session.close()
+    return render_template('items/show.html', item=item)
 
 
 if __name__ == '__main__':
-    app.secret_key = "soham"
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
